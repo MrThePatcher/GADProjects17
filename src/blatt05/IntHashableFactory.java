7380c54f96c2762203b4381a228391c6450f73b2
Patrik <Patrik@192.168.2.105>
2017-06-08 18:25:39 +0200

    new package for homework

---
+++ src/blatt05/IntHashableFactory.java
@@ -0,0 +1,8 @@
+package blatt05;
+public class IntHashableFactory implements HashableFactory<Integer> {
+
+  @Override public DoubleHashable<Integer> create (int size) {
+    return new DoubleHashInt(size);
+  }
+
+}
